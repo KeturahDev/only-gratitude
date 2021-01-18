671d0666d50ecee522d5b41dde161adbf1376455
Keturah Howard <keturah.dev@gmail.com>
2021-01-17 18:58:35 -0800

    install grommet components and make/render header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {Heading} from 'grommet'
+
+class Header extends React.Component {
+  render() {
+    return(
+      <Heading style={{maxWidth: 1500}}>
+        Only Gratitude
+      </Heading>
+    )
+  }
+}
+
+export default Header
